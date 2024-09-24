24bba551163924a6b6521451020efdcd3bbe7499
Udayan Maurya <edmauryaishu@gmail.com>
2024-09-23 18:47:05 -0700

    install util

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { addition, multiply } from "utils";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  console.log(addition(3, 4));
+  console.log(multiply(3, 4));
 
   return (
     <>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
