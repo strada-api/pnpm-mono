372e4d2740c248232ccb9d1c3ad8cb874a88e9fc
Udayan Maurya <edmauryaishu@gmail.com>
2024-09-24 09:57:48 -0700

    add express server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,26 @@
+import cors from "cors";
+import express from "express";
+import { addition, multiply } from "utils";
+
+const app = express();
+
+// Core Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/_health", async (_, res) => {
+  console.log("heare");
+  // Healthcheck
+  try {
+    console.log(addition(4, 5));
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+const SERVER_PORT = 3005;
+app.listen(SERVER_PORT, () =>
+  console.log(`Server ready at: http://localhost:${SERVER_PORT}`)
+);
